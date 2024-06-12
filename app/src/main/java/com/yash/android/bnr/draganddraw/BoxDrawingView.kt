069d07d36f640fca5aa3df7b5b9a84deaca03547
Yashwanth Gajji <gajjiyashwanth7@gmail.com>
2024-06-11 22:50:53 -0400

    Box and handling touch event

--- app/src/main/java/com/yash/android/bnr/draganddraw/BoxDrawingView.kt
+++ app/src/main/java/com/yash/android/bnr/draganddraw/BoxDrawingView.kt
@@ -10,24 +10,41 @@ import android.view.View
 public const val TAG = "BoxDrawingView"
 class BoxDrawingView(context: Context, attrs: AttributeSet?= null) : View(context, attrs) {
 
+    private var currentBox: Box? = null
+    private val boxes = mutableListOf<Box>()
+
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val current = PointF(event.x, event.y)
         var action = ""
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 action = "ACTION_DOWN"
+                currentBox = Box(current).also {
+                    boxes.add(it)
+                }
             }
             MotionEvent.ACTION_UP -> {
                 action = "ACTION_UP"
+                updateCurrentBox(current)
+                currentBox = null
             }
             MotionEvent.ACTION_MOVE -> {
                 action = "ACTION_MOVE"
+                updateCurrentBox(current)
             }
             MotionEvent.ACTION_CANCEL -> {
                 action = "ACTION_CANCEL"
+                currentBox = null
             }
         }
         Log.i(TAG, "$action at x=${current.x}, y=${current.y}")
         return true
+    }
+
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
     }
 }
